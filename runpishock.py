b6d20162b624a2d5706d56f83fd9469ae4da6f6f
poprox24 <hudec.matyas.2006@gmail.com>
2025-09-11 03:41:06 +0200

    Forgot to add logic for customizing serial port of pishock, I hate this

--- runpishock.py
+++ runpishock.py
@@ -65,9 +65,8 @@ trigger_timestamps = []
 last_trigger_time = 0
 
 # Pishock Vars
-pishock_api = SerialAPI(port=None) if USE_PISHOCK else None
+pishock_api = SerialAPI(port=SERIAL_PORT if SERIAL_PORT is not "" else None) if USE_PISHOCK else None
 shocker = None
-
 
 # ~~~      OSC / SERIAL SETUP      ~~~
 osc_sender = None
