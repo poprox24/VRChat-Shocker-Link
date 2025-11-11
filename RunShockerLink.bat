e286e6f9d23d4a4414a569ad939555b1382a1c8f
poprox24 <hudec.matyas.2006@gmail.com>
2025-11-11 14:10:20 +0100

    Improve loading time

--- RunShockerLink.bat
+++ RunShockerLink.bat
@@ -16,6 +16,14 @@ call UpdateScripts.bat
 
 echo [%~n0] Installing requirements (Requires python 3.11+ ideally)
 python -m pip install -r Requirements.txt -q
+if exist vrchat_oscquery (
+    cd vrchat_oscquery
+    git pull
+    cd ..
+) else (
+    git clone https://github.com/theepicsnail/vrchat_oscquery.git
+)
+pip install ./vrchat_oscquery -q
 
 echo [%~n0] Running Shocker Link...
 python VRChatShockerLink.py
--- UpdateScripts.bat
+++ UpdateScripts.bat
@@ -2,4 +2,12 @@
 echo [%~n0] Checking for updates...
 python -m pip install --upgrade pip -q
 python -m pip install -r Requirements.txt -q
+if exist vrchat_oscquery (
+    cd vrchat_oscquery
+    git pull
+    cd ..
+) else (
+    git clone https://github.com/theepicsnail/vrchat_oscquery.git
+)
+pip install ./vrchat_oscquery -q
 python Updatecheck.py
